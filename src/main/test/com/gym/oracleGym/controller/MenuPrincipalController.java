933a9a370b88cc887ac3625e348a49f8c20b0569
root <root@localhost.localdomain>
2020-03-12 17:32:30 -0500

    MRG:enlazados

--- src/main/test/com/gym/oracleGym/controller/MenuPrincipalController.java
+++ src/main/test/com/gym/oracleGym/controller/MenuPrincipalController.java
@@ -1,5 +1,19 @@
 package com.gym.oracleGym.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gym.oracleGym.constant.ViewConstant;
+
+@Controller
 public class MenuPrincipalController {
+	@GetMapping("/menu-principal")
+	public ModelAndView getMenuPrincipal(){
+		ModelAndView mav = new ModelAndView(ViewConstant.MENU_PRINCIPAL);
+
+		return mav;
+	}
+	
 
 }
